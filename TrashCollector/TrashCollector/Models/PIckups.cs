005b5ead6bff7985adcc354cf294333652585dc8
Kimbornino <kimbornino@gmail.com>
2018-10-01 13:25:12 -0500

    Performed migration to add UserRole.

--- TrashCollector/TrashCollector/Models/PIckups.cs
+++ TrashCollector/TrashCollector/Models/PIckups.cs
@@ -7,13 +7,13 @@ namespace TrashCollector.Models
 {
     public class Pickups
     {
-        public int PickupID { get; set; }
-
-        public bool ConfirmPickup { get; set; }
+        public int PickupID { get; set; }   
 
         public int CustomerID { get; set; }
 
         public int EmployeeID { get; set; }
+
+        public bool ConfirmPickup { get; set; }
 
     }
 }
--- TrashCollector/TrashCollector/Startup.cs
+++ TrashCollector/TrashCollector/Startup.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TrashCollector.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
 namespace TrashCollector
 {
     public partial class Startup
     {
+       
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolls();
+
+        }
+        private void CreateRolls()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new Microsoft.AspNet.Identity.RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
         }
     }
 }

9af62213b8299462847e45d98a3580ab8c98e156
Kimbornino <kimbornino@gmail.com>
2018-09-30 19:46:57 -0500

    Added customer class and properties.

---
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Zip { get; set; }
+
+        public DateTime PickupDate {get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}

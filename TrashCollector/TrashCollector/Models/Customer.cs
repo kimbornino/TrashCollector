052fbc30059d50a2cf9f5d0c75edd67d5926dfd1
Kimbornino <kimbornino@gmail.com>
2018-10-02 11:11:56 -0500

    Added a new migration because I updated customer model display names.

--- TrashCollector/TrashCollector/Models/Customer.cs
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -13,21 +13,24 @@ namespace TrashCollector.Models
         public int CustomerId { get; set; }
 
         public string Name { get; set; }
-
         public string Address { get; set; }
 
+        [Display(Name = "Zip Code")]
         public double CustomerZip { get; set; }
 
+        [Display(Name = "Day for Pickup (Monday-Friday)")]
         public string DayOfWeek {get; set; }
 
         public DateTime PickupStartDate { get; set; }
 
         public DateTime PickupEndDate { get; set; }
 
+        [Display(Name = "Select 'Customer' or 'Employee'")]
         [ForeignKey("ApplicationUser")]
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
 
+        [Display(Name = "Your Bill")]
         public double BillAmount { get; set; }
 
     }
